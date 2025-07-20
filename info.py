75f75234daff4184f05a150db6a6815d7da2bc81
rajdev9101 <devraj93957@gmail.com>
2025-07-20 10:47:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -24,8 +24,8 @@ MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+u4cmm3JmIrFlNz
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002052558994').split()]
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://devraj93957:ADKaEq5wfLzGy3JS@cluster0.bhhirpv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
-DATABASE_NAME = environ.get('DATABASE_NAME', "mr.dev")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'mr.dev')
+DATABASE_NAME = environ.get('DATABASE_NAME', "dev")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'dev')
 LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1002052558994'))
 QR_CODE = environ.get('QR_CODE', '')
 START_IMG = environ.get('START_IMG', '')
